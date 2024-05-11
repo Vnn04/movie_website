cae003817268726149054c77f11089e34d63ab77
vnn04 <nguyenvanthan3001@gmail.com>
2024-05-11 07:13:04 +0700

    change add_data.sql

--- database/add_data.sql
+++ database/add_data.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS mlops;
 USE mlops;
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS Users (
     id INT,
     username VARCHAR(255),
     fullname VARCHAR(255),
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS user (
 );
 
 LOAD DATA INFILE '/var/lib/mysql-files/user.csv'
-INTO TABLE user
+INTO TABLE Users
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
@@ -27,8 +27,8 @@ SET bought_vip = CASE
     ELSE 0
 END;
 
-CREATE TABLE IF NOT EXISTS movie (
-    movieID INT,
+CREATE TABLE IF NOT EXISTS Movies (
+    id INT,
     title VARCHAR(255),
     overview TEXT,
     poster_path VARCHAR(255),
@@ -55,11 +55,11 @@ CREATE TABLE IF NOT EXISTS movie (
     War INT,
     Western INT,
     cast TEXT,
-    PRIMARY KEY(movieID)
+    PRIMARY KEY(id)
 );
 
 LOAD DATA INFILE '/var/lib/mysql-files/movie.csv'
-INTO TABLE movie
+INTO TABLE Movies
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
@@ -70,8 +70,8 @@ CREATE TABLE IF NOT EXISTS rating (
     movieID INT,
     rating INT,
     PRIMARY KEY(userID, movieID),
-    FOREIGN KEY (userID) REFERENCES user(id),
-    FOREIGN KEY (movieID) REFERENCES movie(movieID)
+    FOREIGN KEY (userID) REFERENCES Users(id),
+    FOREIGN KEY (movieID) REFERENCES Movies(id)
 );
 
 LOAD DATA INFILE '/var/lib/mysql-files/rating.csv'
@@ -81,17 +81,17 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-CREATE TABLE IF NOT EXISTS view (
+CREATE TABLE IF NOT EXISTS Viewed (
     userID INT,
     movieID INT,
     view INT,
     PRIMARY KEY(userID, movieID),
-    FOREIGN KEY (userID) REFERENCES user(id),
-    FOREIGN KEY (movieID) REFERENCES movie(movieID)
+    FOREIGN KEY (userID) REFERENCES Users(id),
+    FOREIGN KEY (movieID) REFERENCES Movies(id)
 );
 
 LOAD DATA INFILE '/var/lib/mysql-files/view.csv'
-INTO TABLE view
+INTO TABLE Viewed
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
@@ -102,8 +102,8 @@ CREATE TABLE IF NOT EXISTS watch_trailer (
     movieID INT,
     watch INT,
     PRIMARY KEY(userID, movieID),
-    FOREIGN KEY (userID) REFERENCES user(id),
-    FOREIGN KEY (movieID) REFERENCES movie(movieID)
+    FOREIGN KEY (userID) REFERENCES Users(id),
+    FOREIGN KEY (movieID) REFERENCES Movies(id)
 );
 
 LOAD DATA INFILE '/var/lib/mysql-files/watch_trailer.csv'
@@ -112,3 +112,19 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+CREATE TABLE IF NOT EXISTS Visits (
+    id INT AUTO_INCREMENT,
+    userID INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (userID) REFERENCES Users(id)
+);
+
+CREATE TABLE IF NOT EXISTS Histories (
+    id INT AUTO_INCREMENT,
+    userID INT,
+    movieID INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (userID) REFERENCES Users(id),
+    FOREIGN KEY (movieID) REFERENCES Movies(id)
+);
